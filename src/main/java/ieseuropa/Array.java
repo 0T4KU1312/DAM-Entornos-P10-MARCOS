c096cf6f10d54594dc8d3c447e94db471fb812b5
Carol <106676148+Mel-Gr@users.noreply.github.com>
2023-03-16 12:58:12 +0100

    Añadido el método mediana

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,25 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	public static double mediana (int [] a) {
+		Arrays.sort(a);
+		double mediana = 0;
+		if (a.length % 2 != 0) {
+			mediana = a[(a.length/2)];
+		}else {
+			int n = a.length / 2;
+			int m = a.length / 2 +1;
+			mediana = (n+m) / 2;
+		}
+		return mediana;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		int [] a = {3, 6, 2, 7, 4, 8}
+		System.out.println(mediana(a));
 	}
 
 }
