105419c0e6da1c8602476894358e2a33910b70aa
Carol <106676148+Mel-Gr@users.noreply.github.com>
2023-03-17 16:28:53 +0100

    Update Numeros.java

--- src/main/java/ieseuropa/Numeros.java
+++ src/main/java/ieseuropa/Numeros.java
@@ -3,7 +3,7 @@ package main.java.ieseuropa;
 public class Numeros {
   
   public static void numerosPell (){
-		int n1=1, n2=0, pell;
+	int n1=1, n2=0, pell;
         for (int i=0; i<15; i++) {
             pell = n1 + 2 * n2;
             System.out.print(pell + "-");
