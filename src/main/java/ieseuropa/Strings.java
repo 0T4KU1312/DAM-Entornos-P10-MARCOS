83381f709fe9d3088d504167ce11fbe7049226c8
Jorge <Jorge@DESKTOP-IJOFI7U>
2023-03-22 11:24:49 +0100

    Método para calcular cuántas veces se repite una letra.

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -7,9 +7,18 @@ public class Strings {
       System.out.print (a + " " + b + " ");
     }
   }
+  private static int numOcurrencias(String nombre, char letra) {
+		int num = 0;
+		for (int i = 0; i < nombre.length(); i++) {
+			if(nombre.charAt(i) == letra) num++;
+		}
+		return num;
+	}
   public static void main(String[] args) {
 		// TODO Auto-generated method stub
     repeticionStrings("hola", "caracola", 3);
+    char letra = 'o';
+	System.out.println("La letra " + letra + " se repite " + numOcurrencias(nombre, letra) + " veces en el nombre " + nombre + ".");
   }
 
 }
