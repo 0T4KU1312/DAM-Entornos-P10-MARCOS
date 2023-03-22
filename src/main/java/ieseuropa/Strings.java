db8f450d0a77daad196452b30bb27e685b514286
Carol <106676148+Mel-Gr@users.noreply.github.com>
2023-03-22 12:48:16 +0100

    Añadido el metodo de duplicacion de letras

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -7,9 +7,20 @@ public class Strings {
       System.out.print (a + " " + b + " ");
     }
   }
+	
+  public static String repeticionLetras (String a){
+	String b = "";
+	for (int i = 0; i < a.length(); i++) {
+		b += a.charAt(i);
+		b += a.charAt(i);
+	}
+	return b;
+  }
+	
   public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    // TODO Auto-generated method stub
     repeticionStrings("hola", "caracola", 3);
+    System.out.println("\n" + repeticionLetras("NBA"));
   }
 
 }
