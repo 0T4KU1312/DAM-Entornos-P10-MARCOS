0d714349a34a1c58aa2ef9e3eb41be5c9fb8a78f
Jorge <Jorge@DESKTOP-IJOFI7U>
2023-03-22 11:21:57 +0100

    Métodos para convertir un String en mayúsculas y minúsculas.

--- src/main/java/ieseuropa/Strings.java
+++ src/main/java/ieseuropa/Strings.java
@@ -7,9 +7,16 @@ public class Strings {
       System.out.print (a + " " + b + " ");
     }
   }
+  private static String mayus(String nombre) {
+		return nombre.toUpperCase();
+	}
+	private static String minus(String nombre) {
+		return nombre.toLowerCase();
+	}
   public static void main(String[] args) {
 		// TODO Auto-generated method stub
     repeticionStrings("hola", "caracola", 3);
+    System.out.println("El nombre " + nombre + " con todo letras mayúsculas " + mayus(nombre) + " y  con todo letras minúsculas " + minus(nombre) + ".");
   }
 
 }
