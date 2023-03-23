ccdf01d01676fa59b73611c65a9dc9f176ecfc77
Rocio <Rocio@DESKTOP-7D79RVN>
2023-03-23 13:30:13 +0100

    Metodo calcular minimo añadido.

--- src/main/java/ieseuropa/CalcularMin.java
+++ src/main/java/ieseuropa/CalcularMin.java
@@ -1,16 +1,22 @@
 package main.java.ieseuropa;
 
 public class CalcularMin {
+	
 
-	public static void main(String[] args) {
-		int[] numeros = {228,9, 26, 16, 87,65,47,3,56,47};
+	private static int calcularMin(int [] numeros) {
 		int min = numeros[0]; 
 		for (int i = 1; i < numeros.length; i++) {
 		    if (numeros[i] < min) {
 		        min = numeros[i]; 
+		       
 		    }
-		}
-		System.out.println("El mínimo es: " + min);
+		} return min;
 	}
 
+	public static void main(String[] args) {
+		int[] numeros = {228,9, 26, 16, 87,65,47,3,56,108};
+		
+		System.out.println("El mínimo es: " + calcularMin(numeros));
+	
+	}
 }
