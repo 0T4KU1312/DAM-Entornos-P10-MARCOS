da89b6af2e823fad7cc37789ee5942757fbd6cc5
Rocio <Rocio@DESKTOP-7D79RVN>
2023-03-23 13:31:34 +0100

    Metodo calcular media añadido

--- src/main/java/ieseuropa/CalcularMedia.java
+++ src/main/java/ieseuropa/CalcularMedia.java
@@ -1,18 +1,21 @@
 package main.java.ieseuropa;
 
 public class CalcularMedia {
-
-	public static void main(String[] args) {
-	int[] numeros = {57,20,89,55,23,69};
-		
-		int mayor = 0;
+	
+	private static int calcularMedia(int [] numeros) {
 		int suma = 0;
 		for(int i=0;i<numeros.length;i++) {
-			if(numeros[i] > mayor) {
-				mayor = numeros[i];
-			}
+			
 			suma += numeros[i];
-		}
-		System.out.println("La media es " + suma/numeros.length);
+			
+		}return suma/numeros.length;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] numeros = {228,9, 26, 16, 87,65,47,3,56,108};
+		
+		System.out.println("La media es: " + calcularMedia(numeros));
+
 	}
 }
