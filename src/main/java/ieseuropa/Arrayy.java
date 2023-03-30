a9946b4eb2a3fac5681bd6ce2b14021a55a9a981
0t4ku1312 <danolina1312@protonmail.com>
2023-03-30 16:22:44 +0200

    a

--- src/main/java/ieseuropa/Arrayy.java
+++ src/main/java/ieseuropa/Arrayy.java
@@ -2,16 +2,11 @@ package main.java.ieseuropa;
 
 import java.util.Arrays;
 
-public class Array {
+public class Arrayy {
 	
 	public static void ordenar(int[] a) {
 		Arrays.sort(a);
 	}
 
-	public static void main(String[] args) {
-		
-
-
-	}
 
 }
